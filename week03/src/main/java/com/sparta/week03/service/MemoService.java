ed6d3f6e207c404dcbcd3968e3a1b393b4561d87
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-11-16 22:35:18 +0900

    3주차

---
+++ week03/src/main/java/com/sparta/week03/service/MemoService.java
@@ -0,0 +1,25 @@
+package com.sparta.week03.service;
+
+import com.sparta.week03.domain.Memo;
+import com.sparta.week03.domain.MemoRepository;
+import com.sparta.week03.domain.MemoRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor    //final 필수 요소에대해 생성자를 요청
+@Service
+public class MemoService {
+
+    private final MemoRepository memoRepository;
+
+    @Transactional  //db에 변경값이 들어가도록 하는것
+    public Long update(Long id, MemoRequestDto requestDto) {
+        Memo memo = memoRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        memo.update(requestDto);
+        return memo.getId();
+    }
+}
