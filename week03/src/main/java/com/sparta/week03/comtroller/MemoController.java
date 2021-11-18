88a16c66e7aee6989bfe876bd66b2c02ce04b8ef
goodn911 <90505402+goodn911@users.noreply.github.com>
2021-11-18 15:53:51 +0900

    4주차

--- week03/src/main/java/com/sparta/week03/comtroller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/comtroller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,7 +24,10 @@ public class MemoController {
     }
     @GetMapping("/api/memos")
     public List<Memo> getMemos() {
-        return memoRepository.findAllByOrderByModifiedAtDesc();
+        LocalDateTime start = LocalDateTime.now().minusDays(1);
+        LocalDateTime end = LocalDateTime.now();
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start,end);
+
     }
     @PutMapping("/api/memos/{id}")
     public Long updateMemo(@PathVariable Long id,@RequestBody MemoRequestDto memoRequestDto){
